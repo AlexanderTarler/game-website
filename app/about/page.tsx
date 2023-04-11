2c2d37fe6524c331b05ff4cf870c35e9872f4eba
alex <alexander.tarler@gmail.com>
2023-04-11 12:24:51 +0200

    removed one single quote

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             <main>
                 <div className='about'>
                     <h2>I am a newly graduated fullstack developer living in Sweden.
-                        I have a cat named Lucy, I love to play ttrpg's and am a hell of a baker.
+                        I have a cat named Lucy, I love to play ttrpgs and am a hell of a baker.
                         I also spend a few hours every week in the gym because of the baking.
                     </h2>
                 </div>
